0d9beece040a056799627aadc0bb80ababf23468
Andrew Hall <andrew@usamp.com>
2014-08-25 23:33:19 -0700

    Get the webapp back up and running

--- webapp/src/main/java/org/statefulj/webapp/UserController.java
+++ webapp/src/main/java/org/statefulj/webapp/UserController.java
@@ -2,7 +2,6 @@ package org.statefulj.webapp;
 
 import javax.annotation.Resource;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.servlet.ModelAndView;
 import org.statefulj.framework.core.annotations.StatefulController;
 import org.statefulj.framework.core.annotations.Transition;
--- webapp/src/main/java/org/statefulj/webapp/repo/UserRepository.java
+++ webapp/src/main/java/org/statefulj/webapp/repo/UserRepository.java
@@ -1,8 +1,9 @@
 package org.statefulj.webapp.repo;
 
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.CrudRepository;
 import org.statefulj.webapp.model.User;
 
-public interface UserRepository extends CrudRepository<User, Long> {
+public interface UserRepository extends JpaRepository<User, Long> {
  
 }
