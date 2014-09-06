9fc2cfca6a8d08aec8a99b63bd5281081d318915
Andrew Hall <andrew@usamp.com>
2014-09-06 11:03:12 -0700

    Slight cleanup

--- statefulj-framework/statefulj-framework-demo/src/main/java/org/statefulj/webapp/config/MVCConfig.java
+++ statefulj-framework/statefulj-framework-demo/src/main/java/org/statefulj/webapp/config/MVCConfig.java
@@ -4,11 +4,9 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
-import org.springframework.transaction.annotation.EnableTransactionManagement;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.view.InternalResourceViewResolver;
 import org.springframework.web.servlet.view.JstlView;
-import org.statefulj.framework.core.StatefulFactory;
 
 @EnableWebMvc
 @Configuration
--- statefulj-framework/statefulj-framework-demo/src/main/java/org/statefulj/webapp/config/StatefulJConfig.java
+++ statefulj-framework/statefulj-framework-demo/src/main/java/org/statefulj/webapp/config/StatefulJConfig.java
@@ -2,7 +2,6 @@ package org.statefulj.webapp.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.transaction.annotation.EnableTransactionManagement;
 import org.statefulj.framework.core.StatefulFactory;
 
 @Configuration
