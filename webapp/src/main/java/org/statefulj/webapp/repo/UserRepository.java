23b8f87926602953d190f2a5e6daf8f3a6b2b887
Andrew Hall <andrew@usamp.com>
2014-09-07 14:42:28 -0700

    Get rid of obsolete webapp

--- webapp/src/main/java/org/statefulj/webapp/repo/UserRepository.java
+++
@@ -1,9 +0,0 @@
-package org.statefulj.webapp.repo;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.repository.CrudRepository;
-import org.statefulj.webapp.model.User;
-
-public interface UserRepository extends JpaRepository<User, Long> {
- 
-}
