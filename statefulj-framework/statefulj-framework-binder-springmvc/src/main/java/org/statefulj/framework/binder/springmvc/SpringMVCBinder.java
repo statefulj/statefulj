acbfc72b394cd54d6d01d6ff1ff196a929e91a40
Andrew Hall <andrew@usamp.com>
2014-09-09 18:45:31 -0700

    More consistent variable naming

--- statefulj-framework/statefulj-framework-binder-springmvc/src/main/java/org/statefulj/framework/binder/springmvc/SpringMVCBinder.java
+++ statefulj-framework/statefulj-framework-binder-springmvc/src/main/java/org/statefulj/framework/binder/springmvc/SpringMVCBinder.java
@@ -632,17 +632,17 @@ public class SpringMVCBinder implements EndpointBinder {
 	
 	private List<Method> getMethodsAnnotatedWith(final Class<?> type, final Class<? extends java.lang.annotation.Annotation> annotation) {
 	    final List<Method> methods = new ArrayList<Method>();
-	    Class<?> klass = type;
-	    while (klass != Object.class) { // need to iterated thought hierarchy in order to retrieve methods from above the current instance
-	        // iterate though the list of methods declared in the class represented by klass variable, and add those annotated with the specified annotation
-	        final List<Method> allMethods = new ArrayList<Method>(Arrays.asList(klass.getDeclaredMethods()));       
+	    Class<?> clazz = type;
+	    while (clazz != Object.class) { // need to iterated thought hierarchy in order to retrieve methods from above the current instance
+	        // iterate though the list of methods declared in the class represented by clazz variable, and add those annotated with the specified annotation
+	        final List<Method> allMethods = new ArrayList<Method>(Arrays.asList(clazz.getDeclaredMethods()));       
 	        for (final Method method : allMethods) {
 	            if (annotation == null || method.isAnnotationPresent(annotation)) {
 	                methods.add(method);
 	            }
 	        }
 	        // move to the upper class in the hierarchy in search for more methods
-	        klass = klass.getSuperclass();
+	        clazz = clazz.getSuperclass();
 	    }
 	    return methods;
 	}
