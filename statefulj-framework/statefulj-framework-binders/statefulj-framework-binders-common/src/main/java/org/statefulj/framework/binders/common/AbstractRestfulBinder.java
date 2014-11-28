214633796c7860b184a4822e4f6e109826020a94
statefulj <andrew@statefulj.org>
2014-11-28 15:01:43 -0800

    Jersey support - WIP

---
+++ statefulj-framework/statefulj-framework-binders/statefulj-framework-binders-common/src/main/java/org/statefulj/framework/binders/common/AbstractRestfulBinder.java
@@ -0,0 +1,266 @@
+package org.statefulj.framework.binders.common;
+
+import static org.statefulj.framework.binders.common.utils.JavassistUtils.addResourceAnnotation;
+
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+import javax.servlet.http.HttpServletRequest;
+
+import javassist.CannotCompileException;
+import javassist.ClassPool;
+import javassist.CtClass;
+import javassist.CtField;
+import javassist.CtMethod;
+import javassist.NotFoundException;
+import javassist.bytecode.ConstPool;
+import javassist.bytecode.MethodInfo;
+import javassist.bytecode.ParameterAnnotationsAttribute;
+import javassist.bytecode.annotation.Annotation;
+
+import org.apache.commons.lang3.tuple.ImmutablePair;
+import org.apache.commons.lang3.tuple.Pair;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.core.LocalVariableTableParameterNameDiscoverer;
+import org.statefulj.framework.core.model.FSMHarness;
+
+public abstract class AbstractRestfulBinder {
+
+	private Logger logger = LoggerFactory.getLogger(AbstractRestfulBinder.class);
+	
+	private final String HARNESS_VAR = "harness";
+	private final String CONTROLLER_VAR = "controller";
+	
+	private LocalVariableTableParameterNameDiscoverer parmDiscover = new LocalVariableTableParameterNameDiscoverer();
+
+	protected CtMethod createRequestMethod(
+			CtClass mvcProxyClass, 
+			String requestMethod, 
+			String requestEvent, 
+			Method method, 
+			ClassPool cp) throws NotFoundException {
+		String methodName = ("$_" + requestMethod + requestEvent.replace("/", "_").replace("{", "").replace("}", "")).toLowerCase();
+
+		logger.debug(
+				"Create method {} for {}", 
+				methodName,
+				mvcProxyClass.getSimpleName());
+
+		CtClass returnClass = (method == null) ? CtClass.voidType : cp.get(method.getReturnType().getName());
+		CtMethod ctMethod = new CtMethod(returnClass, methodName, null, mvcProxyClass);
+		return ctMethod;
+	}
+	
+	protected void addRequestParameters(
+			boolean referencesId, 
+			CtMethod ctMethod, 
+			Method method, 
+			ClassPool cp) throws NotFoundException, IllegalArgumentException, IllegalAccessException, InvocationTargetException, CannotCompileException {
+
+		String[] parmNames = (method != null) ? parmDiscover.getParameterNames(method) : null;
+ 		MethodInfo methodInfo = ctMethod.getMethodInfo();
+		ParameterAnnotationsAttribute paramAtrributeInfo = 
+				new ParameterAnnotationsAttribute(
+						methodInfo.getConstPool(), 
+						ParameterAnnotationsAttribute.visibleTag);
+		
+		Annotation[][] paramArrays = null;
+		if (method != null) {
+			
+			int parmIndex = 0;
+			
+			// Does this event reference the stateful object?
+			//
+			int annotationCnt = (referencesId) 
+					? method.getParameterTypes().length 
+					: method.getParameterTypes().length - 1;
+			annotationCnt = Math.max(annotationCnt, 1);
+
+			// Pull the Parameter Annotations from the StatefulController - we're going to skip
+			// over the first two - but then we're going to add a parameter for the HttpServletRequest and "id" parameter
+			//
+			java.lang.annotation.Annotation[][] parmAnnotations = method.getParameterAnnotations();
+			paramArrays = new Annotation[annotationCnt][];
+			
+			// Add an Id parameter at the beginning of the method - this will be 
+			// used by the Harness to fetch the object 
+			//
+			if (referencesId) {
+				paramArrays[parmIndex] = addIdParameter(ctMethod, cp);
+				parmIndex++;
+			}
+			
+			// Add an HttpServletRequest - this will be passed in as a context to the finder/factory methods
+			//
+			paramArrays[parmIndex] = addHttpRequestParameter(ctMethod, cp);
+			parmIndex++;
+			
+			int parmCnt = 0;
+			for(Class<?> parm : method.getParameterTypes()) {
+				
+				// Skip first two parameters - they are the Stateful Object and event String.
+				//
+				if (parmCnt < 2) {
+					parmCnt++;
+					continue;
+				}
+				
+				// Clone the parameter Class
+				//
+				CtClass ctParm = cp.get(parm.getName());
+				
+				// Add the parameter to the method
+				//
+				ctMethod.addParameter(ctParm);
+				
+				// Add the Parameter Annotations to the Method
+				//
+				String parmName = (parmNames != null && parmNames.length > parmCnt) ? parmNames[parmCnt] : null;
+				paramArrays[parmIndex] = createParameterAnnotations(
+						parmName,
+						ctMethod.getMethodInfo(),
+						parmAnnotations[parmCnt],
+						paramAtrributeInfo.getConstPool());
+				parmCnt++;
+				parmIndex++;
+			}
+		} else {
+			// NOOP transitions always a require an object Id
+			//
+			paramArrays = new Annotation[2][];
+			paramArrays[0] = addIdParameter(ctMethod, cp);
+			paramArrays[1] = addHttpRequestParameter(ctMethod, cp);
+		}
+		paramAtrributeInfo.setAnnotations(paramArrays);
+		methodInfo.addAttribute(paramAtrributeInfo);
+	}
+	
+	protected void copyParameters(CtMethod ctMethod, Method method, ClassPool cp) throws NotFoundException, IllegalArgumentException, IllegalAccessException, InvocationTargetException, CannotCompileException {
+		String[] parmNames = (method != null) ? parmDiscover.getParameterNames(method) : null;
+ 		MethodInfo methodInfo = ctMethod.getMethodInfo();
+		ParameterAnnotationsAttribute paramAtrributeInfo = 
+				new ParameterAnnotationsAttribute(
+						methodInfo.getConstPool(), 
+						ParameterAnnotationsAttribute.visibleTag);
+		
+		Annotation[][] paramArrays = new Annotation[method.getParameterTypes().length][];
+		java.lang.annotation.Annotation[][] parmAnnotations = method.getParameterAnnotations();
+		int parmIndex = 0;
+		for(Class<?> parm : method.getParameterTypes()) {
+				
+			// Clone the parameter Class
+			//
+			CtClass ctParm = cp.get(parm.getName());
+			
+			// Add the parameter to the method
+			//
+			ctMethod.addParameter(ctParm);
+			
+			// Add the Parameter Annotations to the Method
+			//
+			String parmName = (parmNames != null && parmNames.length > parmIndex) ? parmNames[parmIndex] : null;
+			paramArrays[parmIndex] = createParameterAnnotations(
+					parmName,
+					ctMethod.getMethodInfo(),
+					parmAnnotations[parmIndex],
+					paramAtrributeInfo.getConstPool());
+			parmIndex++;
+		}
+		paramAtrributeInfo.setAnnotations(paramArrays);
+		methodInfo.addAttribute(paramAtrributeInfo);
+	}
+	
+	protected Annotation[] addHttpRequestParameter(CtMethod ctMethod, ClassPool cp) throws NotFoundException, CannotCompileException {
+		// Map the HttpServletRequest class
+		//
+		CtClass ctParm = cp.get(HttpServletRequest.class.getName());
+		
+		// Add the parameter to the method
+		//
+		ctMethod.addParameter(ctParm);
+		
+		return new Annotation[]{};
+		
+	}
+	
+	protected Pair<String, String> parseMethod(String event) {
+		Matcher matcher = getMethodPattern().matcher(event);
+		if (!matcher.matches()) {
+			throw new RuntimeException("Unable to parse event=" + event);
+		}
+		return new ImmutablePair<String, String>(matcher.group(2), matcher.group(3));
+	}
+
+	protected void addRequestMethodBody(boolean referencesId, CtMethod ctMethod, String event) throws CannotCompileException, NotFoundException {
+		String nullObjId = 
+				(referencesId) 
+				? "\"" 
+				: "\", null";
+		
+		String returnType = ctMethod.getReturnType().getName();
+		
+		String returnStmt = 
+				(returnType.equals("void")) 
+				? ""
+				: "return (" + returnType + ")";
+		
+		String methodBody = "{ " 
+				+ returnStmt
+				+ "$proceed(\"" 
+				+ event 
+				+ nullObjId
+				+ ", $args); }";
+
+		ctMethod.setBody(methodBody, "this." + HARNESS_VAR, "onEvent");
+	}
+	
+	protected void addProxyMethodBody(CtMethod ctMethod, Method method) throws CannotCompileException, NotFoundException {
+		String returnType = ctMethod.getReturnType().getName();
+		
+		String returnStmt = 
+				(returnType.equals("void")) 
+				? ""
+				: "return (" + returnType + ")";
+		
+		String methodBody = "{ " 
+				+ returnStmt
+				+ "$proceed($$); }";
+
+		ctMethod.setBody(methodBody, "this." + CONTROLLER_VAR, method.getName());
+	}
+	
+	protected void addControllerReference(
+			CtClass mvcProxyClass,
+			Class<?> clazz,
+			String beanName, 
+			ClassPool cp) throws NotFoundException, CannotCompileException {
+		CtClass type = cp.get(clazz.getName());
+		CtField field = new CtField(type, CONTROLLER_VAR, mvcProxyClass);
+
+		addResourceAnnotation(field, beanName);
+		
+		mvcProxyClass.addField(field);
+	}
+
+	protected void addFSMHarnessReference(CtClass mvcProxyClass, String fsmHarnessId, ClassPool cp) throws NotFoundException, CannotCompileException {
+		CtClass type = cp.get(FSMHarness.class.getName());
+		CtField field = new CtField(type, HARNESS_VAR, mvcProxyClass);
+
+		addResourceAnnotation(field, fsmHarnessId);
+		
+		mvcProxyClass.addField(field);
+	}
+
+	protected abstract Annotation[] addIdParameter(CtMethod ctMethod, ClassPool cp) throws NotFoundException, CannotCompileException;
+	
+	protected abstract Annotation[] createParameterAnnotations(
+			String parmName, 
+			MethodInfo methodInfo, 
+			java.lang.annotation.Annotation[] annotations, 
+			ConstPool parameterConstPool) throws IllegalArgumentException, IllegalAccessException, InvocationTargetException;
+	
+	protected abstract Pattern getMethodPattern();
+}
--- statefulj-framework/statefulj-framework-binders/statefulj-framework-binders-springmvc/src/main/java/org/statefulj/framework/binders/springmvc/SpringMVCBinder.java
+++ statefulj-framework/statefulj-framework-binders/statefulj-framework-binders-springmvc/src/main/java/org/statefulj/framework/binders/springmvc/SpringMVCBinder.java
@@ -22,23 +22,18 @@ import java.lang.reflect.Method;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
-import java.util.regex.Matcher;
 import java.util.regex.Pattern;
-
-import javax.servlet.http.HttpServletRequest;
 
 import javassist.CannotCompileException;
 import javassist.ClassClassPath;
 import javassist.ClassPool;
 import javassist.CtClass;
-import javassist.CtField;
 import javassist.CtMethod;
 import javassist.NotFoundException;
 import javassist.bytecode.AnnotationsAttribute;
 import javassist.bytecode.ClassFile;
 import javassist.bytecode.ConstPool;
 import javassist.bytecode.MethodInfo;
-import javassist.bytecode.ParameterAnnotationsAttribute;
 import javassist.bytecode.annotation.Annotation;
 import javassist.bytecode.annotation.ArrayMemberValue;
 import javassist.bytecode.annotation.EnumMemberValue;
@@ -46,11 +41,9 @@ import javassist.bytecode.annotation.MemberValue;
 import javassist.bytecode.annotation.StringMemberValue;
 
 import org.apache.commons.lang3.StringUtils;
-import org.apache.commons.lang3.tuple.ImmutablePair;
 import org.apache.commons.lang3.tuple.Pair;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.core.LocalVariableTableParameterNameDiscoverer;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.InitBinder;
@@ -60,12 +53,13 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import static org.statefulj.framework.binders.common.utils.JavassistUtils.*;
+
+import org.statefulj.framework.binders.common.AbstractRestfulBinder;
 import org.statefulj.framework.core.model.EndpointBinder;
-import org.statefulj.framework.core.model.FSMHarness;
 import org.statefulj.framework.core.model.ReferenceFactory;
 
 // TODO : Handle when an action doesn't have either the User or Event parameter
-public class SpringMVCBinder implements EndpointBinder {
+public class SpringMVCBinder extends AbstractRestfulBinder implements EndpointBinder {
 	
 	public final static String KEY = "springmvc";
 
@@ -73,12 +67,7 @@ public class SpringMVCBinder implements EndpointBinder {
 	
 	private final Pattern methodPattern = Pattern.compile("(([^:]*):)?(.*)");
 	
-	private LocalVariableTableParameterNameDiscoverer parmDiscover = new LocalVariableTableParameterNameDiscoverer();
-
 	private final String MVC_SUFFIX = "MVCBinder";
-	
-	private final String HARNESS_VAR = "harness";
-	private final String CONTROLLER_VAR = "controller";
 	
 	private final Class<?>[] proxyable = new Class<?>[] {
 			ExceptionHandler.class, 
@@ -145,28 +134,6 @@ public class SpringMVCBinder implements EndpointBinder {
 		ccFile.addAttribute(attr);
 	}
 	
-	private void addControllerReference(
-			CtClass mvcProxyClass,
-			Class<?> clazz,
-			String beanName, 
-			ClassPool cp) throws NotFoundException, CannotCompileException {
-		CtClass type = cp.get(clazz.getName());
-		CtField field = new CtField(type, CONTROLLER_VAR, mvcProxyClass);
-
-		addResourceAnnotation(field, beanName);
-		
-		mvcProxyClass.addField(field);
-	}
-
-	private void addFSMHarnessReference(CtClass mvcProxyClass, String fsmHarnessId, ClassPool cp) throws NotFoundException, CannotCompileException {
-		CtClass type = cp.get(FSMHarness.class.getName());
-		CtField field = new CtField(type, HARNESS_VAR, mvcProxyClass);
-
-		addResourceAnnotation(field, fsmHarnessId);
-		
-		mvcProxyClass.addField(field);
-	}
-
 	private void addRequestMethods(CtClass mvcProxyClass, Map<String,Method> eventMapping, ClassPool cp) throws IllegalArgumentException, NotFoundException, IllegalAccessException, InvocationTargetException, CannotCompileException {
 		
 		// Build a method for each Event
@@ -248,240 +215,6 @@ public class SpringMVCBinder implements EndpointBinder {
 		mvcProxyClass.addMethod(ctMethod);
 	}
 	
-	private CtMethod createRequestMethod(
-			CtClass mvcProxyClass, 
-			String requestMethod, 
-			String requestEvent, 
-			Method method, 
-			ClassPool cp) throws NotFoundException {
-		String methodName = ("$_" + requestMethod + requestEvent.replace("/", "_").replace("{", "").replace("}", "")).toLowerCase();
-
-		logger.debug(
-				"Create method {} for {}", 
-				methodName,
-				mvcProxyClass.getSimpleName());
-
-		CtClass returnClass = (method == null) ? CtClass.voidType : cp.get(method.getReturnType().getName());
-		CtMethod ctMethod = new CtMethod(returnClass, methodName, null, mvcProxyClass);
-		return ctMethod;
-	}
-	
-	private void addRequestMapping(CtMethod ctMethod, String method, String request) {
-		MethodInfo methodInfo = ctMethod.getMethodInfo();
-		ConstPool constPool = methodInfo.getConstPool();
-
-		AnnotationsAttribute attr = new AnnotationsAttribute(constPool, AnnotationsAttribute.visibleTag);
-		Annotation requestMapping = new Annotation(RequestMapping.class.getName(), constPool);
-		
-		ArrayMemberValue valueVals = new ArrayMemberValue(constPool);
-		StringMemberValue valueVal = new StringMemberValue(constPool);
-		valueVal.setValue(request);
-		valueVals.setValue(new MemberValue[]{valueVal});
-		
-		requestMapping.addMemberValue("value", valueVals);
-		
-		ArrayMemberValue methodVals = new ArrayMemberValue(constPool);
-		EnumMemberValue methodVal = new EnumMemberValue(constPool);
-		methodVal.setType(RequestMethod.class.getName());
-		methodVal.setValue(method);
-		methodVals.setValue(new MemberValue[]{methodVal});
-		
-		requestMapping.addMemberValue("method", methodVals);
-		attr.addAnnotation(requestMapping);
-		methodInfo.addAttribute(attr);
-	}
-	
-	private void addRequestMethodBody(boolean referencesId, CtMethod ctMethod, String event) throws CannotCompileException, NotFoundException {
-		String nullObjId = 
-				(referencesId) 
-				? "\"" 
-				: "\", null";
-		
-		String returnType = ctMethod.getReturnType().getName();
-		
-		String returnStmt = 
-				(returnType.equals("void")) 
-				? ""
-				: "return (" + returnType + ")";
-		
-		String methodBody = "{ " 
-				+ returnStmt
-				+ "$proceed(\"" 
-				+ event 
-				+ nullObjId
-				+ ", $args); }";
-
-		ctMethod.setBody(methodBody, "this." + HARNESS_VAR, "onEvent");
-	}
-	
-	private void addProxyMethodBody(CtMethod ctMethod, Method method) throws CannotCompileException, NotFoundException {
-		String returnType = ctMethod.getReturnType().getName();
-		
-		String returnStmt = 
-				(returnType.equals("void")) 
-				? ""
-				: "return (" + returnType + ")";
-		
-		String methodBody = "{ " 
-				+ returnStmt
-				+ "$proceed($$); }";
-
-		ctMethod.setBody(methodBody, "this." + CONTROLLER_VAR, method.getName());
-	}
-	
-	private void addRequestParameters(
-			boolean referencesId, 
-			CtMethod ctMethod, 
-			Method method, 
-			ClassPool cp) throws NotFoundException, IllegalArgumentException, IllegalAccessException, InvocationTargetException, CannotCompileException {
-
-		String[] parmNames = (method != null) ? parmDiscover.getParameterNames(method) : null;
- 		MethodInfo methodInfo = ctMethod.getMethodInfo();
-		ParameterAnnotationsAttribute paramAtrributeInfo = 
-				new ParameterAnnotationsAttribute(
-						methodInfo.getConstPool(), 
-						ParameterAnnotationsAttribute.visibleTag);
-		
-		Annotation[][] paramArrays = null;
-		if (method != null) {
-			int parmIndex = 0;
-			// Does this event reference the stateful object?
-			//
-			int annotationCnt = (referencesId) 
-					? method.getParameterTypes().length 
-					: method.getParameterTypes().length - 1;
-			annotationCnt = Math.max(annotationCnt, 1);
-
-			// Pull the Parameter Annotations from the StatefulController - we're going to skip
-			// over the first two - but then we're going to add a parameter for the HttpServletRequest and "id" parameter
-			//
-			java.lang.annotation.Annotation[][] parmAnnotations = method.getParameterAnnotations();
-			paramArrays = new Annotation[annotationCnt][];
-			
-			// Add an Id parameter at the beginning of the method - this will be 
-			// used by the Harness to fetch the object 
-			//
-			if (referencesId) {
-				paramArrays[parmIndex] = addIdParameter(ctMethod, cp);
-				parmIndex++;
-			}
-			
-			// Add an HttpServletRequest - this will be passed in as a context to the finder/factory methods
-			//
-			paramArrays[parmIndex] = addHttpRequestParameter(ctMethod, cp);
-			parmIndex++;
-			
-			int parmCnt = 0;
-			for(Class<?> parm : method.getParameterTypes()) {
-				
-				// Skip first two parameters - they are the Stateful Object and event String.
-				//
-				if (parmCnt < 2) {
-					parmCnt++;
-					continue;
-				}
-				
-				// Clone the parameter Class
-				//
-				CtClass ctParm = cp.get(parm.getName());
-				
-				// Add the parameter to the method
-				//
-				ctMethod.addParameter(ctParm);
-				
-				// Add the Parameter Annotations to the Method
-				//
-				String parmName = (parmNames != null && parmNames.length > parmCnt) ? parmNames[parmCnt] : null;
-				paramArrays[parmIndex] = createParameterAnnotations(
-						parmName,
-						ctMethod.getMethodInfo(),
-						parmAnnotations[parmCnt],
-						paramAtrributeInfo.getConstPool());
-				parmCnt++;
-				parmIndex++;
-			}
-		} else {
-			// NOOP transitions always a require an object Id
-			//
-			paramArrays = new Annotation[2][];
-			paramArrays[0] = addIdParameter(ctMethod, cp);
-			paramArrays[1] = addHttpRequestParameter(ctMethod, cp);
-		}
-		paramAtrributeInfo.setAnnotations(paramArrays);
-		methodInfo.addAttribute(paramAtrributeInfo);
-	}
-	
-	private void copyParameters(CtMethod ctMethod, Method method, ClassPool cp) throws NotFoundException, IllegalArgumentException, IllegalAccessException, InvocationTargetException, CannotCompileException {
-		String[] parmNames = (method != null) ? parmDiscover.getParameterNames(method) : null;
- 		MethodInfo methodInfo = ctMethod.getMethodInfo();
-		ParameterAnnotationsAttribute paramAtrributeInfo = 
-				new ParameterAnnotationsAttribute(
-						methodInfo.getConstPool(), 
-						ParameterAnnotationsAttribute.visibleTag);
-		
-		Annotation[][] paramArrays = new Annotation[method.getParameterTypes().length][];
-		java.lang.annotation.Annotation[][] parmAnnotations = method.getParameterAnnotations();
-		int parmIndex = 0;
-		for(Class<?> parm : method.getParameterTypes()) {
-				
-			// Clone the parameter Class
-			//
-			CtClass ctParm = cp.get(parm.getName());
-			
-			// Add the parameter to the method
-			//
-			ctMethod.addParameter(ctParm);
-			
-			// Add the Parameter Annotations to the Method
-			//
-			String parmName = (parmNames != null && parmNames.length > parmIndex) ? parmNames[parmIndex] : null;
-			paramArrays[parmIndex] = createParameterAnnotations(
-					parmName,
-					ctMethod.getMethodInfo(),
-					parmAnnotations[parmIndex],
-					paramAtrributeInfo.getConstPool());
-			parmIndex++;
-		}
-		paramAtrributeInfo.setAnnotations(paramArrays);
-		methodInfo.addAttribute(paramAtrributeInfo);
-	}
-	
-	private Annotation[] addHttpRequestParameter(CtMethod ctMethod, ClassPool cp) throws NotFoundException, CannotCompileException {
-		// Map the HttpServletRequest class
-		//
-		CtClass ctParm = cp.get(HttpServletRequest.class.getName());
-		
-		// Add the parameter to the method
-		//
-		ctMethod.addParameter(ctParm);
-		
-		return new Annotation[]{};
-		
-	}
-	
-	private Annotation[] addIdParameter(CtMethod ctMethod, ClassPool cp) throws NotFoundException, CannotCompileException {
-		// Clone the parameter Class
-		//
-		CtClass ctParm = cp.get(Long.class.getName());
-		
-		// Add the parameter to the method
-		//
-		ctMethod.addParameter(ctParm);
-		
-		// Add the Parameter Annotations to the Method
-		//
-		MethodInfo methodInfo = ctMethod.getMethodInfo();
-		ConstPool constPool = methodInfo.getConstPool();
-		AnnotationsAttribute attr = new AnnotationsAttribute(constPool, AnnotationsAttribute.visibleTag);
-		Annotation annot = new Annotation(PathVariable.class.getName(), constPool);
-		
-		StringMemberValue valueVal = new StringMemberValue("id", constPool); 
-		annot.addMemberValue("value", valueVal);
-		attr.addAnnotation(annot);
-		
-		return new Annotation[]{ annot };
-	}
-	
 	/**
 	 * Clone all the parameter Annotations from the StatefulController to the Proxy
 	 * 
@@ -492,7 +225,8 @@ public class SpringMVCBinder implements EndpointBinder {
 	 * @throws IllegalAccessException
 	 * @throws InvocationTargetException
 	 */
-	private Annotation[] createParameterAnnotations(
+	@Override
+	protected Annotation[] createParameterAnnotations(
 			String parmName, 
 			MethodInfo methodInfo, 
 			java.lang.annotation.Annotation[] annotations, 
@@ -520,13 +254,57 @@ public class SpringMVCBinder implements EndpointBinder {
 		return ctParmAnnotations.toArray(new Annotation[]{});
 	}
 	
-
-	private Pair<String, String> parseMethod(String event) {
-		Matcher matcher = this.methodPattern.matcher(event);
-		if (!matcher.matches()) {
-			throw new RuntimeException("Unable to parse event=" + event);
-		}
-		return new ImmutablePair<String, String>(matcher.group(2), matcher.group(3));
+	@Override
+	protected Annotation[] addIdParameter(CtMethod ctMethod, ClassPool cp) throws NotFoundException, CannotCompileException {
+		// Clone the parameter Class
+		//
+		CtClass ctParm = cp.get(Long.class.getName());
+		
+		// Add the parameter to the method
+		//
+		ctMethod.addParameter(ctParm);
+		
+		// Add the Parameter Annotations to the Method
+		//
+		MethodInfo methodInfo = ctMethod.getMethodInfo();
+		ConstPool constPool = methodInfo.getConstPool();
+		AnnotationsAttribute attr = new AnnotationsAttribute(constPool, AnnotationsAttribute.visibleTag);
+		Annotation annot = new Annotation(PathVariable.class.getName(), constPool);
+		
+		StringMemberValue valueVal = new StringMemberValue("id", constPool); 
+		annot.addMemberValue("value", valueVal);
+		attr.addAnnotation(annot);
+		
+		return new Annotation[]{ annot };
 	}
 
+	@Override
+	protected Pattern getMethodPattern() {
+		return this.methodPattern;
+	}
+
+	private void addRequestMapping(CtMethod ctMethod, String method, String request) {
+		MethodInfo methodInfo = ctMethod.getMethodInfo();
+		ConstPool constPool = methodInfo.getConstPool();
+
+		AnnotationsAttribute attr = new AnnotationsAttribute(constPool, AnnotationsAttribute.visibleTag);
+		Annotation requestMapping = new Annotation(RequestMapping.class.getName(), constPool);
+		
+		ArrayMemberValue valueVals = new ArrayMemberValue(constPool);
+		StringMemberValue valueVal = new StringMemberValue(constPool);
+		valueVal.setValue(request);
+		valueVals.setValue(new MemberValue[]{valueVal});
+		
+		requestMapping.addMemberValue("value", valueVals);
+		
+		ArrayMemberValue methodVals = new ArrayMemberValue(constPool);
+		EnumMemberValue methodVal = new EnumMemberValue(constPool);
+		methodVal.setType(RequestMethod.class.getName());
+		methodVal.setValue(method);
+		methodVals.setValue(new MemberValue[]{methodVal});
+		
+		requestMapping.addMemberValue("method", methodVals);
+		attr.addAnnotation(requestMapping);
+		methodInfo.addAttribute(attr);
+	}
 }
