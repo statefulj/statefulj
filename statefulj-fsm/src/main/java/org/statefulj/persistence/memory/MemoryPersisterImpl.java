a89d03b7ef9e34e2edde0f405a355a45d9ae5020
apara <apara@standardset.com>
2015-05-15 08:14:24 -0700

    WIP

--- statefulj-fsm/src/main/java/org/statefulj/persistence/memory/MemoryPersisterImpl.java
+++ statefulj-fsm/src/main/java/org/statefulj/persistence/memory/MemoryPersisterImpl.java
@@ -17,15 +17,16 @@
  */
 package org.statefulj.persistence.memory;
 
-import java.lang.reflect.Field;
-import java.util.List;
-import java.util.concurrent.ConcurrentHashMap;
-import java.util.concurrent.ConcurrentMap;
-
 import org.statefulj.common.utils.ReflectionUtils;
 import org.statefulj.fsm.Persister;
 import org.statefulj.fsm.StaleStateException;
 import org.statefulj.fsm.model.State;
+
+import java.lang.reflect.Field;
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 /**
  * Thread safe, in memory Persister.  
@@ -35,15 +36,14 @@ import org.statefulj.fsm.model.State;
  */
 public class MemoryPersisterImpl<T> implements Persister<T> {
 	
-	private ConcurrentMap<String, State<T>> states = new ConcurrentHashMap<String, State<T>>();
+	private final Map<String, State<T>> states = new HashMap<String, State<T>>();
 	private State<T> start;
 	private String stateFieldName;
+    private volatile Field stateField;
 
-	public MemoryPersisterImpl(List<State<T>> states, State<T> start) {
-		this.start = start;
-		for(State<T> state : states) {
-			this.states.put(state.getName(), state);
-		}
+	public MemoryPersisterImpl(final Collection<State<T>> states, final State<T> start) {
+		setStart(start);
+		setStates(states);
 	}
 	
 	public MemoryPersisterImpl(List<State<T>> states, State<T> start, String stateFieldName) {
@@ -61,19 +61,39 @@ public class MemoryPersisterImpl<T> implements Persister<T> {
 		this.setCurrent(stateful, start);
 	}
 	
-	public ConcurrentMap<String, State<T>> getStates() {
-		return states;
+	public Collection<State<T>> getStates() {
+		return states.values();
 	}
 
-	public void setStates(ConcurrentMap<String, State<T>> states) {
-		this.states = states;
+	public synchronized State<T> addState(final State<T> state) {
+		return states.put(state.getName(), state);
+	}
+
+	public State<T> removeState(final State<T> state) {
+		return removeState(state.getName());
+	}
+
+	public synchronized State<T> removeState(final String name) {
+		return states.remove(name);
+	}
+
+	public void setStates(final Collection<State<T>> states) {
+		//Clear the map
+		//
+		states.clear();
+
+		//Add new states
+		//
+		for(final State<T> state : states) {
+			this.states.put(state.getName(), state);
+		}
 	}
 
 	public State<T> getStart() {
 		return start;
 	}
 
-	public void setStart(State<T> start) {
+	public void setStart(final State<T> start) {
 		this.start = start;
 	}
 
@@ -122,39 +142,46 @@ public class MemoryPersisterImpl<T> implements Persister<T> {
 		}
 	}
 
-	private Field getStateField(T stateful) {
-		Field field = null;
-		
-		// If a state field name was provided, retrieve by name
-		//
-		if (this.stateFieldName != null && !this.stateFieldName.equals("")) {
-			try {
-				field = stateful.getClass().getDeclaredField(this.stateFieldName);
-			} catch (Exception e) {
-				try {
-					field = stateful.getClass().getField(this.stateFieldName);
-				} catch (Exception ee) {
-					// Ignore
-				}
-			}
-		}
-		
-		// Else, fetch the field by Annotation
-		//
-		else {
-			field = ReflectionUtils.getFirstAnnotatedField(stateful.getClass(), org.statefulj.persistence.annotations.State.class);
-			if (field != null) {
-				this.stateFieldName = field.getName();
-			}
-		}
-		
-		if (field == null) {
-			throw new RuntimeException("Unable to locate a State field for stateful: " + stateful);
-		}
-		
-		// Ensure that we can access the field
-		//
-		field.setAccessible(true);
-		return field;
+	private Field getStateField(final T stateful) {
+        if (stateField == null)
+            stateField = locateStateField(stateful);
+        return
+            stateField;
 	}
+
+    private synchronized Field locateStateField(final T stateful) {
+        Field field = null;
+
+        // If a state field name was provided, retrieve by name
+        //
+        if (this.stateFieldName != null && !this.stateFieldName.equals("")) {
+            try {
+                field = stateful.getClass().getDeclaredField(this.stateFieldName);
+            } catch (Exception e) {
+                try {
+                    field = stateful.getClass().getField(this.stateFieldName);
+                } catch (Exception ee) {
+                    // Ignore
+                }
+            }
+        }
+
+        // Else, fetch the field by Annotation
+        //
+        else {
+            field = ReflectionUtils.getFirstAnnotatedField(stateful.getClass(), org.statefulj.persistence.annotations.State.class);
+            if (field != null) {
+                this.stateFieldName = field.getName();
+            }
+        }
+
+        if (field == null) {
+            throw new RuntimeException("Unable to locate a State field for stateful: " + stateful);
+        }
+
+        // Ensure that we can access the field
+        //
+        field.setAccessible(true);
+        return field;
+    }
 }
